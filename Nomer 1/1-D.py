efb038a05d9c6eb40d25ddc1ca8ea601bc9aeed1
R. Yudo Arya Kusuma <72661288+ryudoarya@users.noreply.github.com>
2022-03-11 08:27:55 +0700

    Add files via upload

---
+++ Nomer 1/1-D.py
@@ -0,0 +1,37 @@
+# Nama  : R. Yudo Arya Kusuma
+# NIM   : 20051397013
+# Kelas : D4 MI 2020 A
+# Praktikum 1 Grafika Komputer
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sysD
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-6, 6, -6, 6, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glColor3f(1, 1, 1)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(2.00, 3.00)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(2.00, 5.00)
+    glEnd()
+
+    glutSwapBuffers()
+
+
+glutInit(sys.argv)
+glutInitWindowSize(500, 500)
+glutCreateWindow("Praktikum 1 Grafika Komputer - No. 1-D")
+glutDisplayFunc(draw)
+glutMainLoop()
